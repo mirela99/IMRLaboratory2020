f7939202a7cdda38ec7e0648f8bceb00465383d3
mirela99 <adamache.mirela@yahoo.com>
2020-10-07 13:52:12 +0300

    Change
    attack and walk

---
+++ HW-Week1-IMR/Assets/AnimeControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimeControl : MonoBehaviour
+{
+
+    private Animator mAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool walking = Input.GetKey(KeyCode.W);
+
+        mAnimator.SetBool("walking", walking);
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            mAnimator.SetTrigger("attack");
+        }
+    }
+}
---
+++ HW-Week1-IMR/Assets/Scripts/AttackZombie.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AttackZombie : MonoBehaviour
+{
+    public Camera cameraAr;
+    float distance;
+    public Animator animZombie;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animZombie = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(cameraAr.GetComponent<Transform>().position, gameObject.GetComponent<Transform>().position);
+
+        if(distance < 300.50f)
+        {
+            animZombie.SetBool("Attacking", true);
+            animZombie.SetBool("Skip", false);
+        }
+        else
+        {
+            animZombie.SetBool("Attacking", false);
+            animZombie.SetBool("Skip", true);
+        }
+    }
+}
